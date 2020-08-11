a5cc997649421a9db3661f959dd42691e8bbb88a
alonisser <alonisser@gmail.com>
2020-08-12 00:11:00 +0300

    corona view

--- api/server.py
+++ api/server.py
@@ -1,9 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/') # I'm a route
+
+@app.route('/')  # I'm a route
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/corona')
+def corona_view():
+    return "Over 600 death in Israel currently"
+
 
 if __name__ == "__main__":
     app.run(port=5000, host="127.0.0.1")
