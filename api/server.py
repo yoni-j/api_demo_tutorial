3f05388fe86d90c9dd20ee9c266ba89791875f6f
alonisser <alonisser@gmail.com>
2020-08-12 00:26:25 +0300

    a bit broken api view

--- api/server.py
+++ api/server.py
@@ -1,3 +1,6 @@
+from urllib.parse import urljoin
+
+import requests
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,12 +11,12 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/corona')
+@app.route('/corona')  # I can also be a string
 def corona_view():
     return "Over 600 death in Israel currently"
 
 
-@app.route("/greet/<string:name>")
+@app.route("/greet/<string:name>")  # I can get params
 def greet_view(name: str):
     return f"Hello {name}"
 
@@ -24,6 +27,14 @@ def data_view():
         "corona_dead": [{"date": "11/06/2020", "count": 10},
                         {"date": "12/06/2020", "count": 11}]
     }
+
+
+@app.route("/world_clock/<string:timezone>")
+def world_clock_view(timezone: str):
+    base_uri = "http://worldtimeapi.org/api/timezone/"
+    res = requests.get(urljoin(base_uri, timezone))
+    print(res.json())
+    return f"in {timezone} it's currently {res.json()}"
 
 
 if __name__ == "__main__":
